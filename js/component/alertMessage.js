836e550a14316d8aaa49c52fcfc84c10b8f6f425
raficraft <raficraft@gmail.com>
2022-01-23 13:00:14 +0100

    clean code

--- js/component/alertMessage.js
+++ js/component/alertMessage.js
@@ -1,11 +1,8 @@
-class CreateAlertMessage{
-
-    constructor(type,message){
-
-        this.elHTML = 
-        `<div id="alertMessage--${type}" class="alertMessage alertMessage--${type}">
+class CreateAlertMessage {
+  constructor(type, message) {
+    this.elHTML = `<div id="alertMessage--${type}" class="alertMessage alertMessage--${type}">
             <p>${message}</p>
             <i class="far fa-times-circle"></i>
-        </div>`
-    }
+        </div>`;
+  }
 }
